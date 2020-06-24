04213b4f1caec294ee11bc2ecdd8bbe8401bb99f
Luis Ochoa <eduardo.leo8a@gmail.com>
2020-06-23 20:45:56 -0500

    adding some stuff

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const server = require('./config/server');
 const app = new Express();
 server(app);
 
-app.listen(app.get('port'), () => {
-  console.log(`The app is listening on the port ${app.get('port')}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`The app is listening on the port 3000`);
 });
+
+module.exports = app;
