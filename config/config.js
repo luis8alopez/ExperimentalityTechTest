cc392fe062d08bf8f90949137b09eafdb4367e39
Luis Ochoa <eduardo.leo8a@gmail.com>
2020-06-23 22:03:02 -0500

    trying to fix mongo and heroku issue

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@
    DB_HOST
  } = process.env;
 
-const MONGODB_OPTIONS = { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology:true};
+const MONGODB_OPTIONS = { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology:false};
 const MONGO_URI = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}`;
 const port = process.env.DEV ? process.env.PORT : 5000;
 const morganMode = process.env.DEV ? 'dev' : 'tiny';
